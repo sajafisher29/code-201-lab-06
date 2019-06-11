251a8c470769fdf8c67439fc56e287c07d2ee53b
sajafisher29 <sajafisher@gmail.com>
2019-06-11 08:54:00 -0700

    Working on developing css for homepage

--- js/app.js
+++ js/app.js
@@ -231,3 +231,9 @@ var pageUpdate = function () {
 };
 
 pageUpdate();
+
+var today = newDate();
+var year = today.getFullYear();
+
+var el = document.getElementById('footer');
+el.innerHTML = '<p>Copyright &copy;' + year + '</P>';
